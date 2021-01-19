f70a862d18e25678ef1e7762cf39a49a03fe1860
sumaiya46 <72760514+sumaiya46@users.noreply.github.com>
2021-01-19 18:01:13 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-if input.sound_level():
-    music.ba_ding.play()
-    
+#while True: 
+  #  if input.sound_level()>10:
+   #     light.set_all (color.rgb(0, 0, 255))
+  #  else:
+  #      light.clear()
 
+
+while True:
+    print ("Sound Level:" + input.light_level())
+    if input.light_level()>=5:    
+        music.pew_pew.play_until_done()
+    else:
+        music.magic_wand.play_until_done()
+       
+        
+        
+#while True:
+ #   print ("Sound Level:" + input.light_level()
+ #  if input.light_level()>5:
+  #      music.wawawawaa.play()
+     #   light.set_all (color.rgb(0, 0, 255))
+  # else: 
+      #  music.siren.play()
+      # light.set_all (color.rgb(255, 255, 255))
+#while True:
+   # print ("Sound Level:" + input.sound_level()) 
+    #music.play_melody("G4 G4 A3 G4 C1 B2 G4 G4 A3 G4 D2 C1 G4 G4 G5 E3 C1 B2 A3 F4 F4 E3 C1 D2 C1", 120)
+    #light.show_animation(light.rainbowAnimation,120) 
--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-if (input.soundLevel()) {
-    music.baDing.play()
+// while True: 
+//   if input.sound_level()>10:
+//      light.set_all (color.rgb(0, 0, 255))
+//   else:
+//       light.clear()
+while (true) {
+    console.log("Sound Level:" + input.lightLevel())
+    if (input.lightLevel() >= 5) {
+        music.pewPew.playUntilDone()
+    } else {
+        music.magicWand.playUntilDone()
+    }
+    
 }
-
